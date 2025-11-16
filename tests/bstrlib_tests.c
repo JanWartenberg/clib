20564a84e29d388c30ff5c41906fac50a1a7c121
Jan Wartenberg <jan.wartenberg@web.de>
2025-11-16 16:53:13 +0100

    chapter36: add 3 bstrlib tests

--- tests/bstrlib_tests.c
+++ tests/bstrlib_tests.c
@@ -4,7 +4,6 @@
 
 /*
  * TODO
-    bassign: Set one bstring to another.
     bassignblk: Set a bstring to a C string but give the length.
     bconcat: Concatenate one bstring onto another.
     bstricmp: Compare two bstrings returning the same result as strcmp.
@@ -16,13 +15,14 @@
     blength: Get the length of a bstring.
     bdata: Get the data from a bstring.
     bchar: Get a char from a bstring.
-    
+
     DONE
     bfromcstr: Create a bstring from a C style constant.
     blk2bstr: Do the same thing, but give the length of the buffer.
     bstrcpy: Copy a bstring.
     bassigncstr: Set a bstring to a C string's contents.
     bdestroy: Destroy a bstring.
+    bassign: Set one bstring to another.
  * */
 
 bstring myStr = NULL;
@@ -35,6 +35,61 @@ char *test_create() {
   mu_assert(strcmp(bdata(myStr), "Hello") == 0,
             "bstring has unexpected content");
 
+  return NULL;
+}
+
+char *test_bassign() {
+
+  bstring first = bfromcstr("Hello");
+  bstring second = bfromcstr("World!");
+  int ret;
+
+  ret = bassign(first, second);
+
+  mu_assert(ret == BSTR_OK, "bassign not successful");
+  mu_assert(blength(first) == 6, "bstring has unexpected length");
+  mu_assert(strcmp(bdata(first), "World!") == 0,
+            "bstring has unexpected content");
+  mu_assert(bstrcmp(first, second) == 0, "assigned strings not equal");
+
+  bdestroy(first);
+  bdestroy(second);
+  return NULL;
+}
+
+char *test_bassign_null() {
+  bstring valid = bfromcstr("I'm fine!");
+  int ret;
+
+  // case 1: source NULL
+  ret = bassign(NULL, valid);
+  mu_assert(ret == BSTR_ERR, "bassign should fail when destination is NULL");
+
+  // case 2: source NULL
+  ret = bassign(valid, NULL);
+  mu_assert(ret == BSTR_ERR, "bassign should fail when source is NULL");
+
+  // case 3: both NULL
+  ret = bassign(NULL, NULL);
+  mu_assert(ret == BSTR_ERR, "bassign should fail with both NULL");
+
+  bdestroy(valid);
+  return NULL;
+}
+
+char *test_bassignblk() {
+
+  bstring first = bfromcstr("Hello");
+  char* second = "World!";
+  int ret;
+
+  ret = bassignblk(first, second, 3);
+  mu_assert(ret == BSTR_OK, "bassign not successful");
+  mu_assert(blength(first) == 3, "bstring has unexpected length");
+  mu_assert(strcmp(bdata(first), "Wor") == 0,
+            "bstring has unexpected content");
+
+  bdestroy(first);
   return NULL;
 }
 
@@ -58,6 +113,7 @@ char *test_blk2bstr() {
   mu_assert(strcmp(bdata(mylocalStr), "Hello") == 0,
             "bstring has unexpected content");
 
+  bdestroy(mylocalStr);
   return NULL;
 }
 
@@ -67,9 +123,6 @@ char *test_bstrcpy() {
   bstring mylocalStr2;
   mylocalStr2 = bstrcpy(mylocalStr1);
 
-  printf("blength(myStr): %d\n", blength(mylocalStr1));
-  printf("blength(mylocalStr): %d\n", blength(mylocalStr2));
-
   mu_assert(mylocalStr2 != NULL, "bfromcstr returned NULL");
   mu_assert(blength(mylocalStr2) == 5, "bstring has unexpected length");
 
@@ -78,6 +131,9 @@ char *test_bstrcpy() {
   mu_assert(mylocalStr1 != NULL, "Wrong variable destroyed");
   mu_assert(blength(mylocalStr1) == 5,
             "Previous bstring has unexpected length");
+
+  ret = bdestroy(mylocalStr1);
+  mu_assert(ret == BSTR_OK, "bdestroy not successful");
 
   return NULL;
 }
@@ -95,6 +151,9 @@ char *all_tests() {
   mu_suite_start();
   mu_run_test(test_create);
 
+  mu_run_test(test_bassign);
+  mu_run_test(test_bassign_null);
+  mu_run_test(test_bassignblk);
   mu_run_test(test_bassigncstr);
   mu_run_test(test_blk2bstr);
   mu_run_test(test_bstrcpy);
